f0414e58ec2e0de1c2ab688210edb9e57ef0dcc9
Ana Lima <ayf.lima@usp.br>
2024-08-24 16:10:38 -0300

    feat: add methods to transcribe audio and verbalize text

--- methods/openai_methods.py
+++ methods/openai_methods.py
@@ -1,6 +1,6 @@
+import os
 
 def get_response_from_model(client, question, historical_messages):
-    
     historical_messages.append({"role": "user", "content": question})
     messages_list = [
                 {"role": m["role"], "content": m["content"]}
@@ -14,3 +14,25 @@ def get_response_from_model(client, question, historical_messages):
     response_text = response.choices[0].message.content
     historical_messages.append({"role": "assistant", "content": response_text})
     return response_text
+
+def convert_speech_to_text(client, file_path):
+    audio_file= open(file_path, "rb")
+    transcription = client.audio.transcriptions.create(
+                model="whisper-1",
+                file=audio_file
+        )
+    return transcription.text
+
+def convert_text_to_speech(client, text):
+    if text:
+        speech_file_path = "speech.webm"
+        response = client.audio.speech.create(
+        model="tts-1",
+        voice="echo",
+        input=text
+        )
+        response.stream_to_file(speech_file_path)
+        with open(speech_file_path, "rb") as audio_file:
+            audio_data = audio_file.read()
+        os.remove(speech_file_path)
+        return audio_data
